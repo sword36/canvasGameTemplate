987786479bd56091f2a8e2c1c005fb09c6707845
sword36 <sword36rus@gmail.com>
2015-05-07 22:48:48 +0300

    texture loader and mainLoop

---
+++ canvasGame/js/app.js
@@ -0,0 +1,47 @@
+/**
+ * Created by USER on 07.05.2015.
+ */
+var requestAnimFrame = (function(){
+    return window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+//Create canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+//The main game loop
+var lastTime;
+function main() {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+
+    update(dt);
+    render();
+
+    lastTime = now;
+    requestAnimFrame(main());
+}
+
+function init() {
+    terrainPattern = ctx.createPattern(resources.get("img/terrain.pn"), 'repeat');
+    document.getElementById("play-again").addEventListener("click", function() {
+        reset();
+    });
+    lastTime = Date.now();
+    main();
+}
+resources.load([
+    'img/terrain.png',
+    'img/sprites.png'
+]);
+resources.onReady(init());
