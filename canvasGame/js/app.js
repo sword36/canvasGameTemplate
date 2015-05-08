80e4a0b76a78ed71f3fcfad6143b70339906f014
sword36 <sword36rus@gmail.com>
2015-05-08 16:20:10 +0300

    render add

--- canvasGame/js/app.js
+++ canvasGame/js/app.js
@@ -45,3 +45,19 @@ resources.load([
     'img/sprites.png'
 ]);
 resources.onReady(init());
+
+var player = {
+    pos: [0,0],
+    sprite: new Sprite('img/sprites.png', [0, 0], [39, 39], 16, [0, 1])
+};
+var bullets = [];
+var enemies = [];
+var explosions = [];
+
+var lastFire = Date.now();
+var gameTime = 0;
+var isGameOver;
+var terrainPattern;
+
+var score = 0;
+var scoreEl = document.getElementById('score');
